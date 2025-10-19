d5ba32249d3f4ffd34773cc7459577f05fe57f74
jaskiratt2 <uppjy001@mymail.unisa.edu.au>
2025-10-20 00:02:09 +1030

    Started Hacker class with rig and attack methods. Added hacker identity, inventory and trace leve initialisation. Built in charge aim to bring in a new rig with Crypto Token. launch data spike technique on rig based attacks on target hackers. Print messages included in rig creation, attack actions and trace exposure.

--- Hacker.py
+++ Hacker.py
@@ -6,9 +6,43 @@ ID: <110426141>
 Username: <uppjy001>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from Asset import Asset
+from Rig import Rig
+
 class Hacker:
-    def __init__(self, name, trace_level, inventory, rig):
+    def __init__(self, name ):
         self.name = name
-        self.trace_level = trace_level
-        self.inventory = inventory
-        self.rig = rig
+        self.trace_level = 0
+        self.inventory = [Asset("CrytoToken, either it will acquire or repare the rigs")]
+        self.rig = None
+
+    def acquire_rig(self):
+        for asset in self.inventory:
+            if asset.name == "CrytoToken":
+                self.rig = Rig(self.name + "Rig", 0, False,0, [
+                    Asset(''"Data is Spiking", " gonna launch the attacks"),
+                    Asset("Data is Spiking", " gonna launch the attacks"),
+                    Asset("Removal of Drive", "gonna extract the assets")
+                ])
+                print(f"{self.name}a new rig has been activate named {self.rig.name}")
+                return
+            print(f"{self.name}Sorry, can't acquire a rig because no CrytoToken was found")
+
+    def launch_data_spike(self, target_hacker):
+        if self.rig is None:
+            print(f"{self.name} cannot launch an attack because of no rig")
+            return
+        if target_hacker.rig is None:
+            print(f"{target_hacker.name} no rig to attack")
+            return
+
+        for asset in self.rig.storage:
+            if asset.name == "Data Spike":
+                self.rig.storage.remove(asset)
+                print(f"{self.name} Data Spike launched at {target_hacker.name}")
+                target_hacker.rig.take_hit()
+                self.trace_level += 1
+                if self.trace_level > 5:
+                    print(f"{self.name} It is exposed because of high trace level")
+                return
+        print(f"{self.name} no more Data Spike is left to attack")
