cb44590d458d829c962fdae5f62547d5657d0a06
jaskiratt2 <uppjy001@mymail.unisa.edu.au>
2025-10-19 19:31:14 +1030

    defined both encrypted and decrypted till now.

--- Asset.py
+++ Asset.py
@@ -7,8 +7,22 @@ Username: <uppjy001>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 class Asset:
-    def __init__(self, name, description, encrypted=False):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.encrypted = encrypted
+        self.encrypted = False
+
+    def encrypt(self):
+        if not self.encrypted:
+            self.encrypted = True
+            print(f"{self.name} came out to be encrypted")
+        else:
+            print(f"{self.name} is previously encrypted")
+
+    def decrypt(self):
+        if self.encrypted:
+            self.encrypted = False
+            print(f"{self.name} cam out to be decrypted")
+        else:
+            print(f"{self.name} is previously decrypted")
 
